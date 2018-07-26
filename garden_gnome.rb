09a7f248e77b2ffed13ddc84e5f6d471519e6679
AnyaWall <anyawall@gmail.com>
2018-07-26 19:29:28 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,57 @@
 # Code your instances here
+
+# Code your instances here
+class GardenGnome
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def age=(age)
+    @age=age
+  end
+  
+  def age
+    @age
+  end
+    
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy=gluten_allergy
+  end
+  
+  def gluten_allergy
+    @gluten_allergy
+  end
+  
+  def initialize(personality = "evil", hat_color = "red")
+   @personality=personality
+   @hat_color=hat_color
+ end
+ 
+ def personality
+   @personality
+ end
+ 
+ def hat_color
+   @hat_color
+ end
+ 
+ def gnaw
+   return "Gnawing on a tree!!!"
+ end
+ 
+ def shout
+   return "GNARLY!!!"
+ end
+ 
+ def introduce_self
+   return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+ end
+ end
+ 
+ gnome = GardenGnome.new("Carl the Crappy", "3548")
+  
