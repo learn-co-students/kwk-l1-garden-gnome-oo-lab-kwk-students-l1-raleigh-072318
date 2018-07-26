c9033c5d25ce2522ab63594e2daa8d55dd8d0035
jlongmire02 <jlongmire02@gmail.com>
2018-07-26 19:50:33 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,15 @@
 # Code your instances here
+
+class GardenGnome
+  def initialize (name, age, attribute, personality, hat_color)
+    @name = name
+    @age = age
+    @attribute = attribute
+    @personality = personality
+    @hat_color = hat_color
+  end
+  
+  gnome1 = GardenGnome.new(Walter_the_Worst)
+  gnome2 = GardenGnome.new(James_the_Jerk, 3421)
+  gnome3 = GardenGnome.new(Alfred_the_Abhorrent, 579, gluten_allergy)
+    
